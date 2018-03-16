07e4d4797a0739b6acf924787fb5c5fdb7543a3b
Jason Woods <devel@jasonwoods.me.uk>
2018-03-16 17:27:15 +0000

    Initial commit 1.0.0

---
+++ README.md
@@ -0,0 +1,31 @@
+# Cookie Cutter for Magento
+
+## Overview
+
+Cookie Cutter will cleanup duplicated cookies on a visitor's machine to prevent them from encountering login issues.
+
+This can happen if there was a previous bad cookie domain configuration. For example, if the cookie domain was originally set to example.com and is then changed to www.example.com, login can become broken for some users as Magento will read from one cookie but write to the other.
+
+## Installation
+
+The recommended installation method is to use composer. Alternatively, you can just copy the files into your Magento installation as you would any other extension.
+
+Add the following to your `composer.json` and then run `composer require driskell/magento-cookiecutter`.
+
+```json
+    "repositories": [
+        ...
+        {
+            "type": "vcs",
+            "url": "https://github.com/driskell/magento-cookiecutter"
+        }
+    ]
+```
+
+## Configuration
+
+The following configuration options are currently available in System > Configuration > Driskell > Cookie Cutter.
+
+Option | Description
+--- | ---
+Enable | Enables the cookie cutter
---
+++ app/code/community/Driskell/CookieCutter/Model/Observer.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @copyright See LICENCE.md
+ * @package   Driskell_Daemon
+ * @author    Jason Woods <devel@jasonwoods.me.uk>
+ */
+
+/**
+ * Observer class
+ */
+class Driskell_CookieCutter_Model_Observer
+{
+    /**
+     * Trigger removal of duplicated cookies on every page load
+     *
+     * @param Varien_Observer $observer
+     * @return void
+     */
+    public function triggerCookieCutter(Varien_Observer $observer)
+    {
+        $this->cleanupDuplicatedCooies();
+    }
+
+    /**
+     * Remove duplicated cookies
+     *
+     * @return void
+     */
+    private function cleanupDuplicatedCooies()
+    {
+        if (!isset($_SERVER['HTTP_COOKIE']) || !isset($_SERVER['HTTP_HOST'])) {
+            return;
+        }
+
+        $cookies = array();
+        $duplicateCookies = array();
+        $cookieSplit = preg_split('/;\\s*/', $_SERVER['HTTP_COOKIE']);
+        foreach ($cookieSplit as $cookieString) {
+            $keyValue = explode('=', $cookieString, 2);
+            if (isset($cookies[$keyValue[0]])) {
+                $duplicateCookies[] = $keyValue[0];
+            } else {
+                $cookies[$keyValue[0]] = isset($keyValue[1]) ? $keyValue[1] : '';
+            }
+        }
+
+        if (!$duplicateCookies) {
+            return;
+        }
+
+        $hostSplit = explode('.', $_SERVER['HTTP_HOST']);
+        if (count($hostSplit) < 2) {
+            return;
+        }
+
+        $hostList[] = $currentHost = implode('.', array_reverse(array(array_pop($hostSplit), array_pop($hostSplit))));
+        foreach (array_reverse($hostSplit) as $key) {
+            $hostList[] = $currentHost = $key . '.' . $currentHost;
+        }
+
+        foreach ($duplicateCookies as $key) {
+            Mage::log(
+                sprintf('Removing duplicated cookie %s from hosts %s', $key, implode(', ', $hostList)),
+                null,
+                'cookiecutter.log'
+            );
+            foreach ($hostList as $host) {
+                // NOTE: Should we load the path from configuration?
+                setcookie($key, '', time() - 3600, '/', $host);
+            }
+        }
+    }
+}
